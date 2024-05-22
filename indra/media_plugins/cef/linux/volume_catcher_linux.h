69f76b786dd736d0b6fb2dabb2629746c1f8b1b9
AiraYumi <aira.youme@airanyumi.net>
2024-05-21 20:52:38 -0400

    fix "lines starting with tabs found"

--- indra/media_plugins/cef/linux/volume_catcher_linux.h
+++ indra/media_plugins/cef/linux/volume_catcher_linux.h
@@ -52,83 +52,83 @@ extern "C" {
 class VolumeCatcherPulseAudio : public VolumeCatcherImpl
 {
 public:
-	VolumeCatcherPulseAudio();
-	~VolumeCatcherPulseAudio();
+    VolumeCatcherPulseAudio();
+    ~VolumeCatcherPulseAudio();
 
-	void setVolume(F32 volume);
-	void setPan(F32 pan);
-	void pump();
+    void setVolume(F32 volume);
+    void setPan(F32 pan);
+    void pump();
 
-	// for internal use - can't be private because used from our C callbacks
+    // for internal use - can't be private because used from our C callbacks
 
-	bool loadsyms(std::string pa_dso_name);
-	void init();
-	void cleanup();
+    bool loadsyms(std::string pa_dso_name);
+    void init();
+    void cleanup();
 
-	void update_all_volumes(F32 volume);
-	void update_index_volume(U32 index, F32 volume);
-	void connected_okay();
+    void update_all_volumes(F32 volume);
+    void update_index_volume(U32 index, F32 volume);
+    void connected_okay();
 
-	std::set<U32> mSinkInputIndices;
-	std::map<U32,U32> mSinkInputNumChannels;
-	F32 mDesiredVolume;
-	pa_glib_mainloop *mMainloop;
-	pa_context *mPAContext;
-	bool mConnected;
-	bool mGotSyms;
+    std::set<U32> mSinkInputIndices;
+    std::map<U32,U32> mSinkInputNumChannels;
+    F32 mDesiredVolume;
+    pa_glib_mainloop *mMainloop;
+    pa_context *mPAContext;
+    bool mConnected;
+    bool mGotSyms;
 };
 
 class VolumeCatcherPipeWire : public VolumeCatcherImpl
 {
 public:
-	VolumeCatcherPipeWire();
-	~VolumeCatcherPipeWire();
+    VolumeCatcherPipeWire();
+    ~VolumeCatcherPipeWire();
 
-	bool loadsyms(std::string pw_dso_name);
-	void init();
-	void cleanup();
+    bool loadsyms(std::string pw_dso_name);
+    void init();
+    void cleanup();
 
-	// some of these should be private
+    // some of these should be private
 
-	void lock();
-	void unlock();
+    void lock();
+    void unlock();
 
-	void setVolume(F32 volume);
-	void setPan(F32 pan);
-	void pump();
+    void setVolume(F32 volume);
+    void setPan(F32 pan);
+    void pump();
 
-	void handleRegistryEventGlobal(
-		uint32_t id, uint32_t permissions, const char* type,
-		uint32_t version, const struct spa_dict* props
-	);
+    void handleRegistryEventGlobal(
+        uint32_t id, uint32_t permissions, const char* type,
+        uint32_t version, const struct spa_dict* props
+    );
 
-	class ChildNode
-	{
-	public:
-		bool mActive = false;
+    class ChildNode
+    {
+    public:
+        bool mActive = false;
 
-		pw_proxy* mProxy = nullptr;
-		spa_hook mNodeListener {};
-		spa_hook mProxyListener {};
-		VolumeCatcherPipeWire* mImpl = nullptr;
+        pw_proxy* mProxy = nullptr;
+        spa_hook mNodeListener {};
+        spa_hook mProxyListener {};
+        VolumeCatcherPipeWire* mImpl = nullptr;
 
-		void updateVolume();
-		void destroy();
-	};
+        void updateVolume();
+        void destroy();
+    };
 
-	bool mGotSyms = false;
+    bool mGotSyms = false;
 
-	F32 mVolume = 1.0f; // max by default
-	// F32 mPan = 0.0f; // center
+    F32 mVolume = 1.0f; // max by default
+    // F32 mPan = 0.0f; // center
 
-	pw_thread_loop* mThreadLoop = nullptr;
-	pw_context* mContext = nullptr;
-	pw_core* mCore = nullptr;
-	pw_registry* mRegistry = nullptr;
-	spa_hook mRegistryListener;
+    pw_thread_loop* mThreadLoop = nullptr;
+    pw_context* mContext = nullptr;
+    pw_core* mCore = nullptr;
+    pw_registry* mRegistry = nullptr;
+    spa_hook mRegistryListener;
 
-	std::unordered_set<ChildNode*> mChildNodes;
-	std::mutex mChildNodesMutex;
+    std::unordered_set<ChildNode*> mChildNodes;
+    std::mutex mChildNodesMutex;
     std::mutex mCleanupMutex;
 };
 
